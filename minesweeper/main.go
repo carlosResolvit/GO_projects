a9136e5b35c00efacdbcf66552f8498b47e736cb
Valeria Vera Kröger <Valeria.Vera@resolvit.com>
2021-12-03 18:03:10 -0300

    second commit

--- minesweeper/main.go
+++ minesweeper/main.go
@@ -6,24 +6,42 @@ import (
 	"time"
 )
 
+type slot struct {
+	value      string
+	isSelected bool
+}
+
 var side int = 9   // side length of the board
 var mines int = 10 // number of mines on the board
 
-func createRandomeTable() [9][9]int {
-	var auxTable [9][9]int
+func createRandomeTable() [9][9]slot {
+	var auxTable [9][9]slot
 	for i := 0; i < 9; i++ {
 		for j := 0; i < 9; i++ {
-			auxTable[i][j] = 0
+			auxTable[i][j].isSelected = false
+			auxTable[i][j].value = "-"
 		}
 	}
-	x1 := rand.NewSource(time.Now().UnixNano())
-	y1 := rand.New(x1)
-	fmt.Println(y1.Intn(9))
+	for i := 0; i < 10; i++ {
+		x1 := rand.NewSource(time.Now().UnixNano())
+		y1 := rand.New(x1)
+		rand1 := y1.Intn(9)
+		rand2 := y1.Intn(9)
+		if auxTable[rand1][rand2].value == "-" {
+			auxTable[rand1][rand2].value = "x"
+			i++
+		}
+	}
 	return auxTable
 }
 
-func printTable(aTable [9][9]int) {
-
+func printTable(aTable [9][9]slot) {
+	for i := 0; i < 9; i++ {
+		for j := 0; i < 9; i++ {
+			fmt.Print(aTable[i][j].value + " ")
+		}
+		fmt.Println(" ")
+	}
 }
 
 func main() {
