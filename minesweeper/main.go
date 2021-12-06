bbdd32e22250fa12f5a4842615f1019ad2ce0302
Valeria Vera Kröger <Valeria.Vera@resolvit.com>
2021-12-06 17:21:14 -0300

    commit 4

--- minesweeper/main.go
+++ minesweeper/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"strconv"
 	"time"
 )
 
@@ -10,8 +11,60 @@ const side int = 9   // side length of the board
 const mines int = 10 // number of mines on the board
 
 type slot struct {
-	value      string
-	isSelected bool
+	value       string
+	isDisplayed bool
+	flag        bool
+}
+
+// Returns True if the cell is valid
+func isValid(i int, j int) bool {
+	return (i >= 0) && (i < side) && (j >= 0) && (j < side)
+}
+
+// Returns True if the cell has a Mine
+func isMine(i int, j int, aTable [side][side]slot) bool {
+	return aTable[i][j].value == "x"
+}
+
+func countAdjacentMines(row int, col int, aBoard [side][side]slot) int {
+
+	count := 0
+
+	//----------- 1st to 8th Neighbours ------------
+	// Only process this cells if are a valids ones
+	if isValid(row-1, col) && isMine(row-1, col, aBoard) {
+		count++
+	}
+
+	if isValid(row+1, col) && isMine(row+1, col, aBoard) {
+		count++
+	}
+
+	if isValid(row, col+1) && isMine(row, col+1, aBoard) {
+		count++
+	}
+
+	if isValid(row, col-1) && isMine(row, col-1, aBoard) {
+		count++
+	}
+
+	if isValid(row-1, col+1) && isMine(row-1, col+1, aBoard) {
+		count++
+	}
+
+	if isValid(row-1, col-1) && isMine(row-1, col-1, aBoard) {
+		count++
+	}
+
+	if isValid(row+1, col+1) && isMine(row+1, col+1, aBoard) {
+		count++
+	}
+
+	if isValid(row+1, col-1) && isMine(row+1, col-1, aBoard) {
+		count++
+	}
+
+	return count
 }
 
 func createRandomeTable() [side][side]slot {
@@ -19,7 +72,8 @@ func createRandomeTable() [side][side]slot {
 	var auxTable [side][side]slot
 	for i := 0; i < side; i++ {
 		for j := 0; j < side; j++ {
-			auxTable[i][j].isSelected = false
+			auxTable[i][j].isDisplayed = false
+			auxTable[i][j].flag = false
 			auxTable[i][j].value = "-"
 		}
 	}
@@ -36,22 +90,73 @@ func createRandomeTable() [side][side]slot {
 		}
 	}
 
-	// Add numeric values
+	// Add numeric values to Table
+	for i := 0; i < side; i++ {
+		for j := 0; j < side; j++ {
+			if !isMine(i, j, auxTable) {
+				count := countAdjacentMines(i, j, auxTable)
+				auxTable[i][j].value = strconv.Itoa(count)
+			}
+		}
+	}
 
 	return auxTable
 }
 
-func printTable(aTable [side][side]slot) {
+func flipSlot(i int, j int, table [side][side]slot) {
+	//if is una bomba?
+	//GAME OVER
+	//else is a number > 0
+	// else is 0
+	//display blank neighbours
+
+}
+
+/*func printTable(aTable [side][side]slot) {
 	for i := 0; i < side; i++ {
 		for j := 0; j < side; j++ {
 			fmt.Print(aTable[i][j].value + " ")
 		}
 		fmt.Println(" ")
 	}
+}*/
+
+func printGameTable(aTable [side][side]slot) {
+	fmt.Println("    1   2   3   4   5   6   7   8   9")
+	for i := 0; i < side; i++ {
+		fmt.Println("  +---+---+---+---+---+---+---+---+---+")
+		fmt.Print(i + 1)
+		fmt.Print(" ")
+		for j := 0; j < side; j++ {
+			if aTable[i][j].isDisplayed {
+				fmt.Print("| " + aTable[i][j].value + " ")
+			} else {
+				fmt.Print("| - ")
+			}
+		}
+		fmt.Print("|")
+		fmt.Println(" ")
+	}
+	fmt.Println("  +---+---+---+---+---+---+---+---+---+")
 }
 
 func main() {
+	gameOver := false
 	table := createRandomeTable()
-	printTable(table)
+	var row int = -1
+	var col int = -1
+	for !gameOver {
+		fmt.Println("Current Table:")
+		printGameTable(table)
+		fmt.Println("Choose a row and a column:")
+		fmt.Scan(&row, &col)
+		if isValid(row, col) {
+			flipSlot(row, col, table)
+		} else {
+			fmt.Println("ERROR")
+		}
+	}
+	//imprimir instrucciones - Ingrese fila y columna:
+	//escanear lo que ingresa el usuario
 
 }
